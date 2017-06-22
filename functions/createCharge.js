764c7f6206296f174bb087e065424e226e1d0c53
Yos Riady <yos.riady@bandlab.com>
2017-06-22 10:14:19 +0800

    Bootstrap project

---
+++ functions/createCharge.js
@@ -0,0 +1,16 @@
+module.exports.handler = (event, context, callback) => {
+  console.log('createCharge');
+  console.log(event);
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
